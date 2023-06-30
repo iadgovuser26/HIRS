e08d4517642aa175e002720143db30eb5b33e006
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2023-06-30 13:25:17 -0400

    added TCGEventLogTest.java

---
+++ HIRS_Utils/src/test/java/hirs/utils/tpm/eventlog/TCGEventLogTest.java
@@ -0,0 +1,40 @@
+package hirs.utils.tpm.eventlog;
+
+//import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+
+//import static org.hamcrest.CoreMatchers.equalTo;
+//import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TCGEventLogTest {
+//class TCGEventLogTest {
+	
+	/*
+	@BeforeClass
+	public static final void setup() {
+		System.out.println("test before class");
+	}
+	*/
+	
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("test this");
+	}
+
+	/*
+	@Test
+	public final void test_2() throws NoSuchFieldError, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public final void test_2() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		//fail("Not yet implemented");
+	}
+	*/
+
+}
