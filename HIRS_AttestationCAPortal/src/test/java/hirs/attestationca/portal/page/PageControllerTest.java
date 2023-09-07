b16b78d827d051b750a202afd2645ca0c316725d
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2023-09-07 10:12:09 -0400

    getting controller tests to work

--- HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/PageControllerTest.java
+++ HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/PageControllerTest.java
@@ -1,18 +1,34 @@
 package hirs.attestationca.portal.page;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
+
 
 /**
  * Base class for PageController tests.
  */
 @WebAppConfiguration
+@ContextConfiguration
+@ExtendWith(SpringExtension.class)
+//@RunWith(SpringRunner.class)
 //@ContextConfiguration(classes = PageTestConfiguration.class)
-public class PageControllerTest extends AbstractTestNGSpringContextTests {
+//public class PageControllerTest extends AbstractJUnit4SpringContextTests {
+public abstract class PageControllerTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -51,7 +67,35 @@ public class PageControllerTest extends AbstractTestNGSpringContextTests {
      * Sets up the test environment.
      */
 //    @BeforeMethod
+    @BeforeEach
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
+    /**
+     * Executes a test to check that the page exists, has the correct name, and basic page data in
+     * its model. Methods annotated with @Test in abstract classes are ignored by Spring.
+     *
+     * @throws Exception if test fails
+     */
+    @Test
+    public final void doTestPageExists() throws Exception {
+        // Add prefix path for page verification
+        String pagePath = "/" + page.getPrefixPath() + page.getViewName();
+        if (page.getPrefixPath() == null) {
+            pagePath = "/" + page.getViewName();
+        }
+        System.out.println("HERE IS THE pagePath: " + pagePath);
+
+        getMockMvc()
+                .perform(MockMvcRequestBuilders.get(pagePath))
+                .andExpect(status().isOk())
+                .andExpect(view().name(page.getViewName()))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/" + page.getViewName() + ".jsp"))
+                .andExpect(model().attribute(PageController.PAGE_ATTRIBUTE, equalTo(page)))
+                .andExpect(model().attribute(
+                        PageController.PAGES_ATTRIBUTE, equalTo(Page.values()))
+                );
+    }
 }
+
---
+++ HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/controllers/DevicePageControllerTest.java
@@ -0,0 +1,78 @@
+package hirs.attestationca.portal.page.controllers;
+
+import hirs.attestationca.persist.entity.manager.CertificateRepository;
+import hirs.attestationca.persist.entity.manager.DeviceRepository;
+import hirs.attestationca.portal.page.PageControllerTest;
+
+import static hirs.attestationca.portal.page.Page.DEVICES;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import hirs.attestationca.persist.entity.userdefined.Device;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests that test the URL End Points of DevicePageController.
+ */
+@WebAppConfiguration
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class DevicePageControllerTest extends PageControllerTest {
+
+
+    private static final String DEVICE_NAME = "Test Device";
+    private static final String DEVICE_GROUP_NAME = "Test Device Group";
+    //private static final String TEST_ENDORSEMENT_CREDENTIAL
+    //        = "/endorsement_credentials/tpmcert.pem";
+    //private static final String TEST_ENDORSEMENT_CREDENTIAL_2
+    //        = "/endorsement_credentials/ab21ccf2-tpmcert.pem";
+    //private static final String TEST_PLATFORM_CREDENTIAL
+    //        = "/platform_credentials/Intel_pc.cer";
+
+    private Device device;
+
+    //@Autowired
+    @MockBean
+    private DeviceRepository deviceRepository;
+    //private final DeviceRepository deviceRepository;
+
+    //@Autowired
+    @MockBean
+    private CertificateRepository certificateRepository;
+    //private final CertificateRepository certificateRepository;
+
+
+    /**
+     * Constructor providing the Page's display and routing specification.
+     */
+    public DevicePageControllerTest() {
+        super(DEVICES);
+    }
+
+
+    /**
+     * Tests retrieving the device list using a mocked device manager.
+     *
+     * @throws Exception if test fails
+     */
+    @Test
+    @Rollback
+    public void getDeviceList() throws Exception {
+        // perform test
+            getMockMvc().perform(MockMvcRequestBuilders
+                            .get("/devices/list"))
+                    .andExpect(status().isOk())
+        //            .andExpect(jsonPath("$.data", hasSize(1)))
+                    .andReturn();
+    }
+
+
+}
+
+
