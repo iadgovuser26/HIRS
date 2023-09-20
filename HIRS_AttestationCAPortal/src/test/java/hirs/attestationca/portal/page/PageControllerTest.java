91ac1305a727eb503b9383b502573f28930e66dc
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2023-09-20 13:54:15 -0400

    added files for DevicePageControllerTest

--- HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/PageControllerTest.java
+++ HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/PageControllerTest.java
@@ -1,18 +1,28 @@
 package hirs.attestationca.portal.page;
 
+import hirs.attestationca.portal.HIRSApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
 
 /**
  * Base class for PageController tests.
  */
-@WebAppConfiguration
-//@ContextConfiguration(classes = PageTestConfiguration.class)
-public class PageControllerTest extends AbstractTestNGSpringContextTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes={ HIRSApplication.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)  // needed to use non-static BeforeAll
+public abstract class PageControllerTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -50,8 +60,35 @@ public class PageControllerTest extends AbstractTestNGSpringContextTests {
     /**
      * Sets up the test environment.
      */
-//    @BeforeMethod
+    @BeforeEach
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
+
+    /**
+     * Executes a test to check that the page exists, has the correct name, and basic page data in
+     * its model. Methods annotated with @Test in abstract classes are ignored by Spring.
+     *
+     * @throws Exception if test fails
+     */
+    @Test
+    public final void doTestPageExists() throws Exception {
+        // Add prefix path for page verification
+        String pagePath = "/" + page.getPrefixPath() + page.getViewName();
+        if (page.getPrefixPath() == null) {
+            pagePath = "/" + page.getViewName();
+        }
+        System.out.println("Page Path: " + pagePath);
+
+        getMockMvc()
+                .perform(MockMvcRequestBuilders.get(pagePath))
+                .andExpect(status().isOk())
+                .andExpect(view().name(page.getViewName()))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/" + page.getViewName() + ".jsp"))
+                .andExpect(model().attribute(PageController.PAGE_ATTRIBUTE, equalTo(page)))
+                .andExpect(model().attribute(
+                        PageController.PAGES_ATTRIBUTE, equalTo(Page.values()))
+                );
+    }
 }
+
