45e0dfcf9af01b917c333dd1af95f444a9c85879
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2023-09-20 15:33:26 -0400

    2nd time trying to commit ApplicationIntegrationTest

--- HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/ApplicationIntegrationTest.java
+++ HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/ApplicationIntegrationTest.java
@@ -1,4 +1,16 @@
 package hirs.attestationca.portal.page;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
 public class ApplicationIntegrationTest {
+
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
 }
