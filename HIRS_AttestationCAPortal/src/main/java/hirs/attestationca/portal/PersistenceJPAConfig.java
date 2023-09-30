20e33c5fe2667f2bc9987789594575616e80c02e
iadgovuser29 <33426478+iadgovuser29@users.noreply.github.com>
2023-09-30 00:37:17 -0400

    Fixing typos

--- HIRS_AttestationCAPortal/src/main/java/hirs/attestationca/portal/PersistenceJPAConfig.java
+++ HIRS_AttestationCAPortal/src/main/java/hirs/attestationca/portal/PersistenceJPAConfig.java
@@ -54,8 +54,8 @@ import java.util.Properties;
         // detects if file exists, if not, ignore errors
         @PropertySource(value = "file:/etc/hirs/aca/application.properties",
                 ignoreResourceNotFound = true),
-				
-		@PropertySource(value = "file:C:/ProgramData/hirs/aca/application.win.properties",
+                
+        @PropertySource(value = "file:C:/ProgramData/hirs/aca/application.win.properties",
                 ignoreResourceNotFound = true)
 })
 @ComponentScan({"hirs.attestationca.portal", "hirs.attestationca.portal.page.controllers", "hirs.attestationca.persist", "hirs.attestationca.persist.entity", "hirs.attestationca.persist.service"})
