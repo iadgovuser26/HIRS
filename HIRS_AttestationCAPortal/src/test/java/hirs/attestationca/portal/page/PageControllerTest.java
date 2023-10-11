94c497eec25b8791ba5a21f939142e13ac0e2b49
iadgovuser58 <124906646+iadgovuser58@users.noreply.github.com>
2023-10-11 12:07:10 -0400

    tried one new annotation

--- HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/PageControllerTest.java
+++ HIRS_AttestationCAPortal/src/test/java/hirs/attestationca/portal/page/PageControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextBeforeModesTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -45,6 +46,7 @@ import static org.hamcrest.Matchers.equalTo;
 //@WebAppConfiguration
 @ContextConfiguration(classes = PageTestConfiguration.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)  // needed to use non-static BeforeAll
+//@SpringJUnitWebConfig(locations = "hirs.attestationca.portal.page.controllers")
 public abstract class PageControllerTest {
 
     @Autowired
